decb77de9996b03d7a39095a9fa54f5aabcf4143
Hackerd-byte <hariprasathbm20@gmail.com>
2025-10-02 23:19:14 +0530

    second commit

--- forms/contact.php
+++ forms/contact.php
@@ -1,38 +1,46 @@
 <?php
-
-$name = $_post['name'];
-$email = $_post['email'];
-$subjact = $_post['subjact'];
-$message = $_post['message'];
-
-require 'assets/vendor/autoload.php';
+require "vendor/autoload.php";
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
-
-$mail = new PHPMailer(true);
-
-$mail->isSMTP();
-$mail->SMTPAuth=true;
-$mail->SMTPDebug=SMTP::DEBUG_SERVER;
-
-$mail->Host='smtp.gmail.com';
-$mail->SMTPSecure= PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port=587;
+use PHPMailer\PHPMailer\Exception;
+if ($_SERVER["REQUEST_METHOD"]==="POST"){
 
 
-$mail->Username='hariprofessional2024@gmail.com';
-$mail->Password='gdei eiqs soyf ndda';
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
 
-$mail->setFrom($email, $name);
+    try{
+        $mail = new PHPMailer(true);
 
-$mail->Subject=$subjact;
-$mail->Body-$message;
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+        $mail->isSMTP();
+        $mail->SMTPAuth = true;
+
+        $mail->Host = "smtp.gmail.com";
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+
+        $mail->Username = "hariprofessional2024@gmail.com";  // dkktudrftfizgbkf
+        $mail->Password = "dkktudrftfizgbkf";
+        $mail->setFrom($email, $name);
+        $mail->addAddress("aniyorgalsih2025@gmail.com", "Hari");
 
 
-$mail->send();
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body = $message;
 
-
-
-
+        $mail->send();
+        //header("Location: sent.html"); 
+    } catch(Exception $e){
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+} /* else{
+    header(("Location: index.html"));
+    exit(0);
+} */
 ?>
